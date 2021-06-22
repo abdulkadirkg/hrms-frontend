f6f8470dd29b162a8488abc7edc0ad7a09796521
AbdulkadirKG <abdulkadir-karadag@hotmail.com>
2021-06-22 21:43:00 +0300

    bug fix

--- src/utils/PreloaderUtil/Preloader.jsx
+++ src/utils/PreloaderUtil/Preloader.jsx
@@ -5,8 +5,8 @@ export default function Preloader() {
     return (
       <div>
         <div className="d-flex justify-content-center">
-          <div class="spinner-grow spinner-grow-sm" role="status">
-            <span class="sr-only">Loading...</span>
+          <div className="spinner-grow spinner-grow-sm" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
         </div>
       </div>
